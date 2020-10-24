be1f31dccdaaaf7914e7f3190961f0008cdc8dcf
Conor <Conorbaker52@gmail.com>
2020-10-24 19:10:04 +0100

    Missing Files

---
+++ lib/Pages/wrapper.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:meet_in_the_middle/Pages/home.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+
+
+    return Home();
+  }
+}
