565c329f33f29b1f177d13d6d5047ac73d50ff7b
Conor <Conorbaker52@gmail.com>
2020-11-25 18:54:38 +0000

    Semi Working Location On Maps

---
+++ lib/Pages/map.dart
@@ -0,0 +1,93 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Google Maps Demo',
+      home: MapSample(),
+    );
+  }
+}
+
+class MapSample extends StatefulWidget {
+  @override
+  State<MapSample> createState() => MapSampleState();
+}
+
+class MapSampleState extends State<MapSample> {
+
+  StreamSubscription _locationSubscription;
+  Location _locationTracker = Location();
+  GoogleMapController _controller;
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(6.2603, 53.3498),
+    zoom: 14.4746,
+  );
+
+  static final CameraPosition _kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(37.43296265331129, -122.08832357078792),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
+
+  void getCurrentLocation() async {
+    try {
+
+      var location = await _locationTracker.getLocation();
+
+
+
+      if (_locationSubscription != null) {
+        _locationSubscription.cancel();
+      }
+
+
+      _locationSubscription = _locationTracker.onLocationChanged().listen((newLocalData) {
+        if (_controller != null) {
+          _controller.animateCamera(CameraUpdate.newCameraPosition(new CameraPosition(
+              bearing: 192.8334901395799,
+              target: LatLng(newLocalData.latitude, newLocalData.longitude),
+              tilt: 0,
+              zoom: 18.00)));
+        }
+      });
+
+    } on PlatformException catch (e) {
+      if (e.code == 'PERMISSION_DENIED') {
+        debugPrint("Permission Denied");
+      }
+    }
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: GoogleMap(
+        mapType: MapType.normal,
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) {
+      _controller = controller;
+    },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.location_searching),
+        onPressed: () {
+          getCurrentLocation();
+        }
+      ),
+    );
+  }
+
+
+}
--- lib/Pages/wrapper.dart
+++ lib/Pages/wrapper.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
 import 'file:///C:/Users/Conor/AndroidStudioProjects/meet_in_the_middle/lib/authenticate/authenticate.dart';
 import 'package:meet_in_the_middle/Pages/home.dart';
+import 'package:meet_in_the_middle/Pages/map.dart';
 import 'package:meet_in_the_middle/models/user.dart';
 import 'package:provider/provider.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
-  final user = Provider.of<User>(context);
-  if(user == null){
-    return Authenticate();
-  }else{
-    return Home();
-  }
+    final user = Provider.of<User>(context);
+    if (user == null) {
+      return Authenticate();
+    } else {
+      return MyApp();
+    }
   }
 }
